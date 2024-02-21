4ed1219fcce89819c5e0a3db53c010e04f2e81b7
Crowdbotics <team@crowdbotics.com>
2024-02-21 20:35:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ep-heritage-46549",
+  url: "https://ep-heritage-46549.botics.co",
+  api: "https://ep-heritage-46549.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
